8cd73e8603c3e9e9b04317c0638da870505e0beb
Musa <Musaxlilov29@gmail.com>
2023-10-01 18:06:24 +0400

    Product heart added

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function focus(e){
   console.log(e.type);
   inputFocusBorder.style.border = "none";
 }
+
+const heartRadioButton = document.querySelector(".heart-icon-container-div").children[0];
+const heartRadioButtonActive = document.querySelector(".heart-icon-container-div").children[1];
+console.log(heartRadioButtonActive);
+// console.log(heartRadioButton);
+heartRadioButton.addEventListener("click" , displayHeartToUI);
+heartRadioButtonActive.addEventListener("click" , removeHeartFromUI);
+
+function displayHeartToUI(){
+  heartRadioButtonActive.style.display = "block";
+}
+
+function removeHeartFromUI(){
+  heartRadioButtonActive.style.display = "none";
+}
